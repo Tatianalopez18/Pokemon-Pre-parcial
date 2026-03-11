ddd4cf915ed7e05d70a9f53f0eaa5c11b7370ebd
Tatianalopez18 <stlopez43@ucatolica.edu.co>
2026-03-10 22:47:08 -0500

    otro commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from pokemons import pokemons
 
-app = FastAPI()
-
+app = FastAPI(
+    title="Pre-parcial Pokemon API",
+    description="API hecha con FastAPI para consultar y enfrentar pokemones de primera generación",
+)
 class Pokemon(BaseModel):
     id: int
     name: str
@@ -13,10 +15,16 @@ class Pokemon(BaseModel):
     type: List[str]
 
     def leave_pokeball(self) -> str:
-        return f"{self.name} salio de la pokebola"
+        return f"{self.name} salió de la pokebola"
+    
 
 all_pokemons = [Pokemon(**pokemon) for pokemon in pokemons]
 
 @app.get ("/showallpokemons/")
 def show_all_pokemons():
     return all_pokemons
+
+@app.get ("/showonepokemon/")
+def show_one_pokemons(name:str = Query(...)):
+    pokemon = nombre_pokemon(name)
+    return one_pokemons 
